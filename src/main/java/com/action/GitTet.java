a0ac361414216a34a9d520aa456b32a83c66ba67
xujianglin <2402208586@qq.com>
2022-10-14 15:22:20 +0800

    master second commit

--- src/main/java/com/action/GitTet.java
+++ src/main/java/com/action/GitTet.java
@@ -3,5 +3,6 @@ package com.action;
 public class GitTet {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
